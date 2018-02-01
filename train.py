641d5876c7f3e2754450a74a208fcac6d34e4a24
Zhuoyue Wang <zhuoyue2@illinois.edu>
2018-02-01 08:04:31 -0600

    weird

--- train.py
+++ train.py
@@ -31,24 +31,20 @@ Your LaTeX file should be called paper.tex and should use the ACL 2017 style and
 import numpy as np
 from matplotlib import pyplot as plt
 import csv
+import random
 
-
-def train(numIter, eta, inputFile):
-    with open(inputFile,'r') as tsv:
-        lines = [line.split(',') for line in tsv]
+def train(numIter, eta, inputFile, first, second):
+    tsv = open(inputFile, "r")
+    lines = tsv.readlines()
+    tsv.close()
+    random.shuffle(lines)
+    lines = [(line.strip()).split(',') for line in lines]
     label = list()
     data = list()
-    versiFlag = 0
     for line in lines:
         length = len(line)
-        if line[length-1] == 'Iris-setosa':
+        if line[length-1] == first:
             label.append(1)
-            versiFlag = 1
-        elif line[length-1] == 'Iris-versicolor':
-            if versiFlag == 0: #versicolor is the first -> 1
-                label.append(1)
-            else:
-                label.append(-1) # the second is versilvollor
         else:
             label.append(-1)
 
@@ -59,7 +55,6 @@ def train(numIter, eta, inputFile):
     data = np.array(data)
     weight = np.zeros(len(data[0]))
 
-
     for perIter in range(numIter):
         for i,x in enumerate(data):
             if data[i].dot(weight)*label[i] <= 0:
@@ -69,22 +64,18 @@ def train(numIter, eta, inputFile):
     return weight
 
 
-def test(weight, inputFile):
-    with open(inputFile,'r') as tsv:
-        lines = [line.strip().split(',') for line in tsv]
+def test(weight, inputFile, first, second):
+    tsv = open(inputFile, "r")
+    lines = tsv.readlines()
+    tsv.close()
+    random.shuffle(lines)
+    lines = [line.strip().split(',') for line in lines]
     label = list()
     data = list()
-    versiFlag = 0
     for line in lines:
         length = len(line)
-        if line[length-1] == 'Iris-setosa':
+        if line[length-1] == first:
             label.append(1)
-            versiFlag = 1
-        elif line[length-1] == 'Iris-versicolor':
-            if versiFlag == 0: #versicolor is the first -> 1
-                label.append(1)
-            else:
-                label.append(-1) # the second is versilvollor
         else:
             label.append(-1)
         temp = [float(line[i]) for i in range(length-1)]
@@ -101,19 +92,24 @@ def test(weight, inputFile):
                 total_error = total_error + np.dot(data[i],weight)*label[i]
                 weight += eta*data[i]*label[i]
         errors.append(total_error*(-1))
+        print("the current error rate is {}".format(total_error*(-1)))
+
+    plt.plot(errors)
+    plt.xlabel('Epoch')
+    plt.ylabel('Total Loss')
 
     return errors
 
 
 def main():
-    weight1 = train(50, 1, "setosa-versicolor.txt")
-    error1 = test([1,1,1,1], "setosa-versicolor.txt")
+    weight1 = train(50, 1, "setosa-versicolor.txt", "Iris-setosa", "Iris-versicolor")
+    error1 = test([1,1,1,1], "setosa-versicolor.txt", "Iris-setosa", "Iris-versicolor")
 
-#    weight2 = train(50, 1, "versicolor-virginica.txt")
-#    error2 = test([1,1,1,1], "versicolor-virginica.txt")
+#    weight2 = train(50, 1, "versicolor-virginica.txt", "Iris-versicolor", "Iris-virginica")
+#    error2 = test([1,1,1,1], "versicolor-virginica.txt", "Iris-versicolor", "Iris-virginica")
 
-#    weight3 = train(50, 1, "setosa-virginica.txt")
-#    error3 = test([1,1,1,1],"setosa-virginica.txt")
+#    weight3 = train(50, 1, "setosa-virginica.txt", "Iris-setosa", "Iris-virginica")
+#    error3 = test([1,1,1,1],"setosa-virginica.txt", "Iris-setosa", "Iris-virginica")
 
 
 
